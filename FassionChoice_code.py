7a00c1c122435df7d23b00713372d4dc321e0f58
arusnmi <parush.larty@gmail.com>
2025-04-15 23:47:16 +0530

    forgot to add the code to the previous commit

--- FassionChoice_code.py
+++ FassionChoice_code.py
@@ -12,3 +12,11 @@ apikey="AIzaSyBYkCnr6h6YxXT_WM3Gk4vD0vVBy-SW9jY"
 
 genai.configure(api_key=apikey) 
 
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
+
+
+
+model=genai.GenerativeModel('gemini-2.5-pro-exp-03-25')
+
