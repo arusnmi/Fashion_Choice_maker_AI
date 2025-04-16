f908ca5da6120dc1552fd2a4b5f29759ae011ed5
arusnmi <parush.larty@gmail.com>
2025-04-16 10:22:45 +0530

    did the first prompt

--- FassionChoice_code.py
+++ FassionChoice_code.py
@@ -12,11 +12,19 @@ apikey="AIzaSyBYkCnr6h6YxXT_WM3Gk4vD0vVBy-SW9jY"
 
 genai.configure(api_key=apikey) 
 
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
+
+
 
 
 
 model=genai.GenerativeModel('gemini-2.5-pro-exp-03-25')
 
+Gender=input("Enter your Gender (Male/female/non binary): ")
+body_type=input("Enter your body type (e.g., athletic, curvy, etc.): ")
+outfit_type=input("Enter the type of outfit you want (e.g., casual, formal, etc.): ")
+occasion=input("Enter the occasion (e.g., wedding, party, etc.): ")
+Clothing=input("Enter the type of cloths that you want to style, eg(chino pants, shirt): ")
+
+response = model.generate_content("What would go with a "+Clothing+" for a "+occasion+" that  is "+outfit_type+" on a "+Gender+" "+body_type+" body type?")
+
+print(response.text)
